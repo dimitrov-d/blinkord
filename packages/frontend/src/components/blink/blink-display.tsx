3c3fc57188de8fd0b38cad89e17c7b1125e073fd
Damjan <damjandimitrov1@gmail.com>
2024-09-01 21:58:35 +0200

    Blink adapts to current site theme

--- packages/frontend/src/components/blink/blink-display.tsx
+++ packages/frontend/src/components/blink/blink-display.tsx
@@ -12,15 +12,18 @@ interface BlinkPreviewProps {
 
 export const BlinkPreview: React.FC<BlinkPreviewProps> = ({ serverId, code }) => {
   const actionApiUrl = `${process.env.NEXT_PUBLIC_API_BASE_URL}/blinks/${serverId}${code ? `?code=${code}` : ''}`;
+
   const { adapter } = useActionSolanaWalletAdapter(process.env.NEXT_PUBLIC_HELIUS_URL!)
   const { action } = useAction({ url: actionApiUrl, adapter })
 
-  if (!action) return null
+  if (!action) return null;
+
+  const stylePreset = localStorage.getItem('isDark') === 'true' ? 'x-dark' : 'default';
 
   return (
     <div className="p-4 rounded-lg ">
       <div className="w-full h-full overflow-hidden rounded-lg">
-        <Blink action={action} websiteText='blinkord.com' />
+        <Blink action={action} websiteText='blinkord.com' stylePreset={stylePreset} />
       </div>
     </div>
   )
