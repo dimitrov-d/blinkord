2d2fe69bba6235a5281524d17b41db829020814c
Damjan <damjandimitrov1@gmail.com>
2024-09-03 21:13:07 +0200

    Server page cleanup & refactor

--- packages/frontend/src/app/servers/page.tsx
+++ packages/frontend/src/app/servers/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState, useContext } from "react";
 import { useRouter } from "next/navigation";
-import { Card, CardContent } from "@/components/ui/card";
+import { CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { useUserStore } from "@/lib/contexts/zustand/userStore";
 import Image from "next/image";
@@ -34,7 +34,9 @@ export default function Servers() {
     }
     const guilds = localStorage.getItem("guilds");
     if (guilds?.length) {
-      setGuilds(JSON.parse(guilds).sort((a: any, b: any) => a.name.localeCompare(b.name)));
+      setGuilds(
+        JSON.parse(guilds).sort((a: any, b: any) => a.name.localeCompare(b.name))
+      );
       setIsFetchingGuilds(false);
       return;
     }
@@ -112,6 +114,106 @@ export default function Servers() {
     }, 100);
   };
 
+  const renderGuildCard = (guild: Guild, index: number) => (
+    <motion.div
+      key={guild.id}
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5, delay: index * 0.1 }}
+      className="flex justify-center"
+    >
+      <SpotlightCard
+        hsl
+        hslMin={200}
+        hslMax={280}
+        from="rgba(255,255,255,0.20)"
+        mode="after"
+        size={400}
+        className={`group w-72 rounded-2xl border-2 ${isDark ? "border-gray-700 bg-zinc-900" : "border-gray-200 bg-white"}`}
+      >
+        <div className={`absolute inset-px rounded-[calc(var(--radius)-1px)] ${isDark ? "bg-black/10" : "bg-white"}`} />
+
+        <div className="h-32 relative rounded">
+          {guild.image ? (
+            <Image
+              src={guild.image}
+              alt=""
+              layout="fill"
+              objectFit="cover"
+              className="opacity-80 blur-sm rounded"
+            />
+          ) : (
+            <>
+              <div className="w-full h-full bg-gradient-to-br from-cyan-200 to-indigo-400 rounded" />
+              <span className="absolute inset-x-0 z-1 bottom-0 h-2  bg-gradient-to-r from-green-400 via-blue-600 to-purple-700" />
+            </>
+          )}
+          <div className="absolute inset-0 bg-black bg-opacity-40 rounded" />
+          <div className="absolute inset-0 flex items-center justify-center">
+            <span className="absolute inset-x-0 z-1 bottom-0 h-2  bg-gradient-to-r from-green-400 via-blue-600 to-purple-700" />
+            {guild.image ? (
+              <Image
+                src={guild.image}
+                alt={guild.name}
+                width={64}
+                height={64}
+                className="rounded-full border-2 border-neon-cyan shadow-md"
+              />
+            ) : (
+              <div className="w-16 h-16 rounded-full bg-cyan-500 flex items-center justify-center text-white text-2xl font-bold border-2 border-white shadow-md">
+                {guild.name.charAt(0)}
+              </div>
+            )}
+          </div>
+        </div>
+        <CardContent className="p-4 relative">
+          <GridPattern
+            offsetX={0}
+            offsetY={0}
+            size={64}
+            className={`absolute -inset-x-0.5 -top-1/4 h-[150%] w-full skew-y-12 ${isDark ? "stroke-gray-600" : "stroke-gray-200"} stroke-[2] [mask-image:radial-gradient(black,transparent_70%)]`}
+          >
+            {[1, 2, 3].map((_, i) => (
+              <GridPattern.Block
+                key={i}
+                row={1 + i}
+                column={i * 2}
+                className="fill-black/5 transition duration-500 hover:fill-black/10"
+              />
+            ))}
+          </GridPattern>
+
+          <div className="relative font-display text-4xl font-bold">
+            <div className="mb-8">
+              <h2 className={`text-xl font-semibold ${isDark ? "text-white" : ""}`}>
+                {guild.name}
+              </h2>
+              <p className={`text-sm ${isDark ? "text-gray-400" : "text-gray-600"}`}>
+                {guild.hasBot ? "Bot Added" : ""}
+              </p>
+            </div>
+          </div>
+          <div className="relative bottom-0 right-0 z-0 flex justify-center">
+            <Button
+              variant="secondary"
+              className={`py-2 px-4 text-sm font-semibold transition-all duration-300 ${guild.hasBot
+                ? "bg-cyan-400 hover:bg-cyan-600 text-white"
+                : "bg-purple-500 hover:bg-purple-600 text-white"
+                }`}
+              onClick={() =>
+                guild.hasBot
+                  ? handleServerSelect(guild.id, guild.name, guild.image)
+                  : handleInstallBot(guild.id)
+              }
+            >
+              {guild.hasBot ? "Configure" : "Add Bot"}
+            </Button>
+          </div>
+        </CardContent>
+      </SpotlightCard>
+    </motion.div>
+  );
+
   return (
     <div className="container mx-auto px-4 py-12">
       <motion.h1
@@ -138,213 +240,7 @@ export default function Servers() {
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5, delay: 0.2 }}
         >
-          {guilds.map((guild, index) => (
-            <motion.div
-              key={guild.id}
-              initial={{ opacity: 0, y: 20 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.5, delay: index * 0.1 }}
-              className="flex justify-center"
-            >
-              {isDark ? (
-                <SpotlightCard
-                  key={index}
-                  hsl
-                  hslMin={200}
-                  hslMax={280}
-                  from="rgba(255,255,255,0.20)"
-                  mode="after"
-                  size={400}
-                  className="group w-72 rounded-2xl border-2 border-gray-700 bg-zinc-900"
-                >
-                  <div className="absolute inset-px rounded-[calc(var(--radius)-1px)] bg-black/10"></div>
-
-                  <div className="h-32 relative rounded">
-                    {guild.image ? (
-                      <Image
-                        src={guild.image}
-                        alt=""
-                        layout="fill"
-                        objectFit="cover"
-                        className="opacity-80 blur-sm rounded"
-                      />
-                    ) : (
-                      <>
-                          <div className="w-full h-full bg-gradient-to-br from-cyan-200 to-indigo-400 rounded" />
-                      <span className="absolute inset-x-0 z-1 bottom-0 h-2  bg-gradient-to-r from-green-400 via-blue-600 to-purple-700"></span>
-
-                      </>
-                  
-                    )}
-                    <div className="absolute inset-0 bg-black bg-opacity-40 rounded" />
-                    <div className="absolute inset-0 flex items-center justify-center">
-                      <span className="absolute inset-x-0 z-1 bottom-0 h-2  bg-gradient-to-r from-green-400 via-blue-600 to-purple-700"></span>
-                      {guild.image ? (
-                        <Image
-                          src={guild.image}
-                          alt={guild.name}
-                          width={64}
-                          height={64}
-                          className="rounded-full border-2 border-neon-cyan shadow-md"
-                        />
-                        
-                      ) : (
-                        <div className="w-16 h-16 rounded-full bg-cyan-500 flex items-center justify-center text-white text-2xl font-bold border-2 border-white shadow-md">
-                          {guild.name.charAt(0)}
-                        </div>
-                      )}
-                    </div>
-                  </div>
-                  <CardContent className="p-4 relative">
-                    <GridPattern
-                      offsetX={0}
-                      offsetY={0}
-                      size={64}
-                      className="absolute -inset-x-0.5 -top-1/4 h-[150%] w-full skew-y-12 stroke-gray-600 stroke-[2] [mask-image:radial-gradient(black,transparent_70%)]"
-                    >
-                      {[1, 2, 3].map((_, i) => (
-                        <GridPattern.Block
-                          key={i}
-                          row={1 + i}
-                          column={i * 2}
-                          className="fill-black/5 transition duration-500 hover:fill-black/10"
-                        />
-                      ))}
-                    </GridPattern>
-
-                    <div className="relative font-display text-4xl font-bold">
-                      <div className="mb-8">
-                        <h2 className="text-xl font-semibold text-white">
-                          {guild.name}
-                        </h2>
-                        <p className="text-sm text-gray-400">
-                          {guild.hasBot ? "Bot Master" : "Owner"}
-                        </p>
-                      </div>
-                    </div>
-                    <div className="relative bottom-0 right-0 z-0 flex justify-center">
-                      <Button
-                        variant="secondary"
-                        className={`py-2 px-4 text-sm font-semibold transition-all duration-300 ${guild.hasBot
-                          ? "bg-cyan-400 hover:bg-cyan-600 text-white"
-                          : "bg-purple-500 hover:bg-purple-600 text-white"
-                          }`}
-                        onClick={() =>
-                          guild.hasBot
-                            ? handleServerSelect(
-                              guild.id,
-                              guild.name,
-                              guild.image
-                            )
-                            : handleInstallBot(guild.id)
-                        }
-                      >
-                        {guild.hasBot ? "Manage" : "Setup"}
-                      </Button>
-                    </div>
-                  </CardContent>
-                </SpotlightCard>
-              ) : (
-                <SpotlightCard
-                  key={index}
-                  hsl
-                  hslMin={200}
-                  hslMax={280}
-                  from="rgba(255,255,255,0.20)"
-                  mode="after"
-                  size={400}
-                  className="group w-72 rounded-2xl border-2 border-gray-200 bg-white"
-                >
-                  <div className="absolute inset-px rounded-[calc(var(--radius)-1px)] bg-white"></div>
-
-                  <div className="h-32 relative rounded">
-                    {guild.image ? (
-                      <Image
-                        src={guild.image}
-                        alt=""
-                        layout="fill"
-                        objectFit="cover"
-                        className="opacity-80 blur-sm rounded"
-                      />
-                    ) : (
-                      <>
-                          <div className="w-full h-full bg-gradient-to-br from-cyan-200 to-indigo-400 rounded" />
-                      <span className="absolute inset-x-0 z-1 bottom-0 h-2  bg-gradient-to-r from-green-400 via-blue-600 to-purple-700"></span>
-
-                      </>
-                  
-                    )}
-                    <div className="absolute inset-0 bg-black bg-opacity-40 rounded" />
-                    <div className="absolute inset-0 flex items-center justify-center">
-                      <span className="absolute inset-x-0 z-1 bottom-0 h-2  bg-gradient-to-r from-green-400 via-blue-600 to-purple-700"></span>
-                      {guild.image ? (
-                        <Image
-                          src={guild.image}
-                          alt={guild.name}
-                          width={64}
-                          height={64}
-                          className="rounded-full border-2 border-neon-cyan shadow-md"
-                        />
-                        
-                      ) : (
-                        <div className="w-16 h-16 rounded-full bg-cyan-500 flex items-center justify-center text-white text-2xl font-bold border-2 border-white shadow-md">
-                          {guild.name.charAt(0)}
-                        </div>
-                      )}
-                    </div>
-                  </div>
-                  <CardContent className="p-4 relative">
-                    <GridPattern
-                      offsetX={0}
-                      offsetY={0}
-                      size={64}
-                      className="absolute -inset-x-0.5 -top-1/4 h-[150%] w-full skew-y-12 stroke-gray-200 stroke-[2] [mask-image:radial-gradient(black,transparent_70%)]"
-                    >
-                      {[1, 2, 3].map((_, i) => (
-                        <GridPattern.Block
-                          key={i}
-                          row={1 + i}
-                          column={i * 2}
-                          className="fill-black/5 transition duration-500 hover:fill-black/10"
-                        />
-                      ))}
-                    </GridPattern>
-
-                    <div className="relative font-display text-4xl font-bold">
-                      <div className="mb-8">
-                        <h2 className="text-xl font-semibold">
-                          {guild.name}
-                        </h2>
-                        <p className="text-sm text-gray-600">
-                          {guild.hasBot ? "Bot Master" : "Owner"}
-                        </p>
-                      </div>
-                    </div>
-                    <div className="relative bottom-0 right-0 z-0 flex justify-center">
-                      <Button
-                        variant="secondary"
-                        className={`py-2 px-4 text-sm font-semibold transition-all duration-300 ${guild.hasBot
-                          ? "bg-cyan-400 hover:bg-cyan-600 text-white"
-                          : "bg-purple-500 hover:bg-purple-600 text-white"
-                          }`}
-                        onClick={() =>
-                          guild.hasBot
-                            ? handleServerSelect(
-                              guild.id,
-                              guild.name,
-                              guild.image
-                            )
-                            : handleInstallBot(guild.id)
-                        }
-                      >
-                        {guild.hasBot ? "Manage" : "Setup"}
-                      </Button>
-                    </div>
-                  </CardContent>
-                </SpotlightCard>
-              )}
-            </motion.div>
-          ))}
+          {guilds.map((guild, index) => renderGuildCard(guild, index))}
         </motion.div>
       )}
     </div>
