f53836d34676366bdacf37f64a628828bb54e9eb
Damjan <damjandimitrov1@gmail.com>
2024-09-03 21:48:35 +0200

    Dark mode style improvements for 404 and redirect

--- packages/frontend/src/app/not-found.tsx
+++ packages/frontend/src/app/not-found.tsx
@@ -3,28 +3,41 @@
 import Image from "next/image";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import { useContext } from "react";
+import { ThemeContext } from "@/lib/contexts/ThemeProvider";
 
 export default function NotFound() {
+  const { isDark } = useContext(ThemeContext);
+
   return (
-    <div className="relative flex h-screen flex-col bg-bg">
-      <div className="grid h-screen place-content-center bg-white px-4">
-        <div className="absolute inset-0 -z-10 flex justify-center items-center"></div>
+    <div
+      className={`relative flex h-screen flex-col ${isDark ? "bg-gray-900" : "bg-bg"
+        }`}
+    >
+      <div
+        className={`grid h-screen place-content-center px-4 ${isDark ? "bg-gray-900" : "bg-white"
+          }`}
+      >
+        <div className="absolute inset-0 -z-10 flex justify-center items-center" />
         <div className="text-center relative z-10">
           <div className="absolute inset-0 -z-10 flex justify-center items-center">
             <motion.div
-              className="absolute top-32 -right-0 w-0 h-0 border-l-[50px] border-l-transparent border-r-[50px] border-r-transparent border-b-[100px] border-b-gray-700 z-10 "
+              className={`absolute top-32 -right-0 w-0 h-0 border-l-[50px] border-l-transparent border-r-[50px] border-r-transparent border-b-[100px] ${isDark ? "border-b-gray-200" : "border-b-gray-700"
+                } z-10`}
               initial={{ opacity: 0, y: -50 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.5, delay: 0.2 }}
             />
             <motion.div
-              className="absolute top-0 left-10 w-0 h-0 border-l-[50px] border-l-transparent border-r-[50px] border-r-transparent border-b-[100px] border-b-yellow-400 z-1 "
+              className={`absolute top-0 left-10 w-0 h-0 border-l-[50px] border-l-transparent border-r-[50px] border-r-transparent border-b-[100px] ${isDark ? "border-b-yellow-300" : "border-b-yellow-400"
+                } z-1`}
               initial={{ opacity: 0, y: -50 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.5, delay: 0.4 }}
             />
             <motion.div
-              className="absolute top-10 left-20 w-64 h-36 bg-indigo-600 z-1"
+              className={`absolute top-10 left-20 w-64 h-36 ${isDark ? "bg-indigo-400" : "bg-indigo-600"
+                } z-1`}
               initial={{ opacity: 0, scale: 0.8 }}
               animate={{ opacity: 1, scale: 1 }}
               transition={{ duration: 0.5, delay: 0.6 }}
@@ -45,7 +58,8 @@ export default function NotFound() {
             />
           </motion.div>
           <motion.h1
-            className="mt-6 text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl"
+            className={`mt-6 text-2xl font-bold tracking-tight sm:text-4xl ${isDark ? "text-gray-200" : "text-gray-900"
+              }`}
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5, delay: 0.2 }}
@@ -53,7 +67,8 @@ export default function NotFound() {
             Uh-oh!
           </motion.h1>
           <motion.h1
-            className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl"
+            className={`text-2xl font-bold tracking-tight sm:text-4xl ${isDark ? "text-gray-200" : "text-gray-900"
+              }`}
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5, delay: 0.4 }}
@@ -61,7 +76,8 @@ export default function NotFound() {
             We can't find that page.
           </motion.h1>
           <motion.h2
-            className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl"
+            className={`text-2xl font-bold tracking-tight sm:text-4xl ${isDark ? "text-gray-200" : "text-gray-900"
+              }`}
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5, delay: 0.6 }}
