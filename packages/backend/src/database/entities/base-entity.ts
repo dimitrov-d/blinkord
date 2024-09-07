9bf45cc2e34c7028e4987723af7da2efb8c7e5a3
Damjan <damjandimitrov1@gmail.com>
2024-09-07 09:25:17 +0200

    Add createTime, updateTime columns

--- packages/backend/src/database/entities/base-entity.ts
+++ packages/backend/src/database/entities/base-entity.ts
@@ -1,5 +1,13 @@
+import { CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
 export class BaseEntity<T> {
   constructor(data: Partial<T>) {
     Object.assign(this, data);
   }
+
+  @CreateDateColumn()
+  createTime: Date;
+
+  @UpdateDateColumn()
+  updateTime: Date;
 }
