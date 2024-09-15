cd38303703bf3697a90453c0c21633f4fa8d5b05
Damjan <damjandimitrov1@gmail.com>
2024-09-15 10:55:16 +0200

    Add Blink skeleton if loading

--- packages/frontend/src/components/skeletons/blink-skeleton.tsx
+++ packages/frontend/src/components/skeletons/blink-skeleton.tsx
@@ -3,12 +3,13 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export function BlinkCardSkeleton() {
   return (
-    <Card className="w-full h-[400px]">
+    <Card className="w-full h-[620px]">
       <CardContent className="p-6">
-        <Skeleton className="h-[200px] w-full mb-4" />
+        <Skeleton className="h-[400px] w-full mb-4" />
         <Skeleton className="h-4 w-3/4 mb-2" />
         <Skeleton className="h-4 w-1/2 mb-4" />
         <Skeleton className="h-10 w-full" />
+        <Skeleton className="h-10 mt-4 w-full" />
       </CardContent>
     </Card>
   );
