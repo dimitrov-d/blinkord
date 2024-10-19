5652db6c75f51adfaa655c1b0d57d9eccf3e255a
Damjan <damjandimitrov1@gmail.com>
2024-10-19 11:26:25 +0200

    Add useEffect to prevent window error

--- packages/frontend/src/app/install-bot/page.tsx
+++ packages/frontend/src/app/install-bot/page.tsx
@@ -1,25 +1,28 @@
 "use client";
 
+import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import Redirect from "../redirect/page";
 
 export default function InstallBot() {
   const router = useRouter();
 
-  const url = new URL(window.location.href);
-  const redirect = url.searchParams.get('redirect');
-  const guild_id = url.searchParams.get('guild_id');
+  useEffect(() => {
+    const url = new URL(window.location.href);
+    const redirect = url.searchParams.get('redirect');
+    const guild_id = url.searchParams.get('guild_id');
 
-  let baseUrl = 'https://discord.com/oauth2/authorize?client_id=1277276051592052787&permissions=268462081&integration_type=0&scope=bot+applications.commands';
+    let baseUrl = 'https://discord.com/oauth2/authorize?client_id=1277276051592052787&permissions=268462081&integration_type=0&scope=bot+applications.commands';
 
-  if (guild_id) baseUrl += `&guild_id=${guild_id}`;
+    if (guild_id) baseUrl += `&guild_id=${guild_id}`;
 
-  if (redirect === 'true') {
-    const redirectUri = encodeURIComponent(`${process.env.NEXT_PUBLIC_APP_BASE_URL!}/redirect`);
-    router.push(`${baseUrl}&redirect_uri=${redirectUri}&response_type=code`);
-  } else {
-    router.push(baseUrl);
-  }
+    if (redirect === 'true') {
+      const redirectUri = encodeURIComponent(`${process.env.NEXT_PUBLIC_APP_BASE_URL!}/redirect`);
+      router.push(`${baseUrl}&redirect_uri=${redirectUri}&response_type=code`);
+    } else {
+      router.push(baseUrl);
+    }
+  }, [router]);
 
   return (
     <Redirect />
