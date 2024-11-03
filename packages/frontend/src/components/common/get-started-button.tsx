10bd075fa1a348dc690889351dc4b25f159f849a
Future Star <160122343+futurestar425@users.noreply.github.com>
2024-11-03 17:47:46 +0300

    Update get-started-button.tsx

--- packages/frontend/src/components/common/get-started-button.tsx
+++ packages/frontend/src/components/common/get-started-button.tsx
@@ -1,11 +1,14 @@
 "use client";
 
-import { useState } from "react";
-import { LogIn } from "lucide-react";
+import { useState, useEffect } from "react";
+import { LogIn, LogOut } from "lucide-react";
 import { Button } from "../ui/button";
+import { useRouter } from "next/navigation";
+import { useLogin, usePrivy } from "@privy-io/react-auth";
 import { cn } from "@/lib/utils";
 
 export const handleConnectDiscord = async () => {
+
   try {
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_API_BASE_URL}/login?owner=true`,
@@ -24,22 +27,47 @@ export const handleConnectDiscord = async () => {
   }
 };
 
+
+
 export default function GetStartedButton({
   className,
 }: {
   className?: string;
 }) {
-  const [loading, setLoading] = useState(false);
 
-  const handleGetStartedClick = async () => {
-    setLoading(true);
-    await handleConnectDiscord();
-    setLoading(false);
-  };
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+  const { login }  = useLogin({
+    onComplete: (user, isNewUser, wasAlreadyAuthenticated, loginMethod, linkedAccount) => {
+      console.log(user, isNewUser, wasAlreadyAuthenticated, loginMethod, linkedAccount);
+      // Any logic you'd like to execute if the user is/becomes authenticated while this
+      // component is mounted
+    },
+    onError: (error) => {
+      console.log(error);
+      // Any logic you'd like to execute after a user exits the login flow or there is an error
+    },
+  });
+ 
+  const {
+    ready,
+    authenticated,
+    logout,
+  } = usePrivy();
+
+  useEffect(() => {
+    if (ready && !authenticated) {
+      router.push("/");
+    }
+    if (ready && authenticated) {
+      router.push("/wallet");
+    }
+    
+  }, [ready, authenticated, router]);
 
   return (
     <Button
-      onClick={handleGetStartedClick}
+      onClick={ready && authenticated ? logout : login}
       className={cn(
         "w-fit bg-builderz-blue hover:bg-neon-cyan text-black font-bold px-6 py-4 rounded-full transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50",
         {
@@ -49,7 +77,18 @@ export default function GetStartedButton({
       )}
       disabled={loading}
     >
-      <LogIn className="mr-2 h-4 w-4 sm:h-5 sm:w-5" /> Get Started
+      {
+        ready && authenticated ? 
+        <div className="flex">
+          Log Out
+          <LogOut className="ml-2 h-4 w-4 sm:h-5 sm:w-5" /> 
+        </div>
+        :
+        <>
+          <LogIn className="mr-2 h-4 w-4 sm:h-5 sm:w-5" /> 
+          Get Started
+        </>
+      }
     </Button>
   );
 }
