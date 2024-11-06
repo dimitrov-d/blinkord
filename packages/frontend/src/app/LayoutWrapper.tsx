24abace1c2282005721de3cecd025674c8ee3960
Damjan <damjandimitrov1@gmail.com>
2024-11-06 21:42:13 +0100

    WIP: embedded wallets

--- packages/frontend/src/app/LayoutWrapper.tsx
+++ packages/frontend/src/app/LayoutWrapper.tsx
@@ -4,7 +4,6 @@ import React, { useMemo } from "react";
 import "../styles/globals.css";
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-// import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 import { clusterApiUrl } from '@solana/web3.js';
 import {
@@ -13,25 +12,16 @@ import {
 } from '@solana/wallet-adapter-wallets';
 import ContextProvider from "@/lib/contexts/ContextProvider";
 import { ThemeProvider } from "@/lib/contexts/ThemeProvider";
+import { PrivyProvider } from "@privy-io/react-auth";
 
 require("@solana/wallet-adapter-react-ui/styles.css");
 
-type LayoutWrapperProps = {
-  children: React.ReactNode;
-};
+type LayoutWrapperProps = { children: React.ReactNode; };
 
 const LayoutWrapper: React.FC<LayoutWrapperProps> = ({ children }) => {
   const network = WalletAdapterNetwork.Mainnet;
-
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
-
-  const wallets = useMemo(
-    () => [
-      new PhantomWalletAdapter(),
-      new SolflareWalletAdapter({ network }),
-    ],
-    [network]
-  );
+  const wallets = useMemo(() => [new PhantomWalletAdapter(), new SolflareWalletAdapter({ network }),], [network]);
 
   return (
     <ConnectionProvider endpoint={endpoint}>
@@ -39,7 +29,25 @@ const LayoutWrapper: React.FC<LayoutWrapperProps> = ({ children }) => {
         <WalletModalProvider>
           <ContextProvider>
             <ThemeProvider>
-              {children}
+              <PrivyProvider
+                appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID!}
+                config={{
+                  appearance: {
+                    accentColor: "#38CCCD",
+                    theme: "#FFFFFF",
+                    landingHeader: 'Log In or Sign In',
+                    loginMessage: "Please login with discord.",
+                    showWalletLoginFirst: false,
+                    logo: "https://auth.privy.io/logos/privy-logo.png",
+                    walletChainType: "solana-only",
+                    walletList: ["detected_solana_wallets", "phantom"]
+                  },
+                  loginMethods: ["discord",],
+                  embeddedWallets: { createOnLogin: "all-users", },
+                }}
+              >
+                {children}
+              </PrivyProvider>
             </ThemeProvider>
           </ContextProvider>
         </WalletModalProvider>
