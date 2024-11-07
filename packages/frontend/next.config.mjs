16bb3ede13fd38f76b623df3c82d9482e1facaf0
Damjan <damjandimitrov1@gmail.com>
2024-11-08 00:28:42 +0100

    Add security headers

--- packages/frontend/next.config.mjs
+++ packages/frontend/next.config.mjs
@@ -8,6 +8,31 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'no-referrer',
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains; preload',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
